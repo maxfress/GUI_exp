4311fc1e0f2db436ceb31a5f5d821c1eea1e29d6
maxfress <121790405+maxfress@users.noreply.github.com>
2023-01-02 09:15:12 +0100

    Update GUI_exp.py

--- GUI_exp.py
+++ GUI_exp.py
@@ -1,13 +1,6 @@
 import streamlit as st
 import altair as alt
 
-from utils.experiment import Experiment
-from etf.models.naive_average import NaiveSeasonalAverage
-from darts.models.forecasting.exponential_smoothing import ExponentialSmoothing
-from testdata.utils.provider import DataProvider
-from darts import TimeSeries
-import pandas as pd
-
 
 alt.themes.enable("streamlit")
 
